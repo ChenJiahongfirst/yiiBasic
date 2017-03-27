7eac93921c9d919b3befbde8076ceafa96249537
ChenJiahongfirst <chen.jiahongfirst@gmail.com>
2017-03-27 20:27:11 +0800

    model->select
    数据模型的建立和从数据库查询数据。

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -3,12 +3,33 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use app\models\Test;
 
 class HelloController extends Controller {
-    
-    public $layout='common';
-    public function actionIndex() {       
-        return $this->render('about');//php文件默认传送到$content中
+
+    public function actionIndex() {
+        //查询数据
+//        $sql = "select * from test where id=:id";
+//        $result=Test::findBySql($sql,array(':id'=>1))->all();
+        
+        //id=1
+//        $result = Test::find()->where(['id' => 1])->all();
+        
+        //id>0
+//        $result = Test::find()->where(['>','id',0])->all();
+        
+        //title like '%...%'
+//        $result = Test::find()->where(['like','title','title'])->all();
+        
+        //查询结果转化为数组
+//        $result = Test::find()->where(['like','title','title'])->asArray()->all();
+        
+        //批量查询
+//        foreach(Test::find()->batch(1) as $test){
+//            print_r($test);
+//        }
+        
+//        print_r($result);
     }
 
 }
