b63d536f253fd569ed0c1ba710348a22ea531bb9
ChenJiahongfirst <chen.jiahongfirst@gmail.com>
2017-03-27 16:51:43 +0800

    controller->cookies
    cookies集合

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -3,23 +3,21 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use yii\web\Cookie;
+
 
 class HelloController extends Controller {
 
     public function actionIndex() {
-        $session=\YII::$app->session;
-        $session->open();
-//        if($session->isActive){
-//            echo 'session is start';
-//        }
-       
-//        $session->set('name','张三');
-//        echo $session->get('name');
-//        $session->remove('name');
-        
-//        $session['name']='李四';
-//        echo $session['name'];
-//        unset($session['name']);
+//        $cookies=\YII::$app->response->cookies;
+        //添加cookie
+//        $cookie_data=array('name'=>'user','value'=>'张三');       
+//        $cookies->add(new Cookie($cookie_data));
+        //移除cookie
+//        $cookies->remove('user');
+        //使用cookie
+//        $cookies=\YII::$app->request->cookies;
+//        echo $cookies->getValue('user','cookie过期');
     }
 
 }
