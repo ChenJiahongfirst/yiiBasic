8a39494227fd119f515b801fb50228320abf04c5
ChenJiahongfirst <chen.jiahongfirst@gmail.com>
2017-03-27 15:44:48 +0800

    controller-response
    响应组件

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -8,15 +8,17 @@ class HelloController extends Controller {
 
     public function actionIndex() {
         //获得请求组件
-        $request = \YII::$app->request;
 
-        //echo $request->get('id','获取id失败！');
-
-/*        if ($request->isGet) {
-            echo 'This is a get method';
-        }
-*/
-//        echo $request->userIp;
+        $res = \YII::$app->response;
+        //状态码
+//        $res->statusCode='404';
+        //请求头
+//        $res->headers->add('pragma','no-cache');
+        //跳转
+//        $this->redirect('http://www.baidu.com',302);
+        //文件下载
+        $res->sendFile('./robots.txt');
+        
     }
 
 }
