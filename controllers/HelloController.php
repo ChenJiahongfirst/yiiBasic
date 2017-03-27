312839399d5ab6d90bba84c09810b366bc0b9343
ChenJiahongfirst <chen.jiahongfirst@gmail.com>
2017-03-27 17:38:22 +0800

    view--$key
    将数据传递到视图

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -8,7 +8,12 @@ use yii\web\Controller;
 class HelloController extends Controller {
 
     public function actionIndex() {
-        return $this->renderPartial('index');
+        $test='test<script>alert(3);</script>';
+        $test_array=array(0,1);
+        $data=array();
+        $data['key_test']=$test;
+        $data['key_test_array']=$test_array;
+        return $this->renderPartial('index',$data);
     }
 
 }
--- views/hello/index.php
+++ views/hello/index.php
@@ -1,2 +1,8 @@
-<h3>Hello World!</h3>
+<?php
+use yii\helpers\Html;
+use yii\helpers\HtmlPurifier;
+?>
+<h3><?=Html::encode($key_test)?></h3>
+<h3><?=HtmlPurifier::process($key_test)?></h3>
+<h3><?=$key_test_array[0]?></h3>
 
