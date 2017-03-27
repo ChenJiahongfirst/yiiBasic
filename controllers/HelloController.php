96fe5bfae09d198b854e64e83bfd69809f652313
ChenJiahongfirst <chen.jiahongfirst@gmail.com>
2017-03-27 17:06:25 +0800

    view->create a new page
    新建一个页面

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -3,21 +3,12 @@
 namespace app\controllers;
 
 use yii\web\Controller;
-use yii\web\Cookie;
 
 
 class HelloController extends Controller {
 
     public function actionIndex() {
-//        $cookies=\YII::$app->response->cookies;
-        //添加cookie
-//        $cookie_data=array('name'=>'user','value'=>'张三');       
-//        $cookies->add(new Cookie($cookie_data));
-        //移除cookie
-//        $cookies->remove('user');
-        //使用cookie
-//        $cookies=\YII::$app->request->cookies;
-//        echo $cookies->getValue('user','cookie过期');
+        return $this->renderPartial('index');
     }
 
 }
