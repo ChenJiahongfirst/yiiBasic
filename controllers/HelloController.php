c70f11bc26ca935d6a2a8f8b67255782c3d9d3cf
ChenJiahongfirst <chen.jiahongfirst@gmail.com>
2017-03-28 15:46:45 +0800

    model->delete

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -8,28 +8,11 @@ use app\models\Test;
 class HelloController extends Controller {
 
     public function actionIndex() {
-        //查询数据
-//        $sql = "select * from test where id=:id";
-//        $result=Test::findBySql($sql,array(':id'=>1))->all();
+        //删除数据
+//        $results = Test::find()->where(['id' => 1])->all();
+//        $results[0]->delete();
         
-        //id=1
-//        $result = Test::find()->where(['id' => 1])->all();
-        
-        //id>0
-//        $result = Test::find()->where(['>','id',0])->all();
-        
-        //title like '%...%'
-//        $result = Test::find()->where(['like','title','title'])->all();
-        
-        //查询结果转化为数组
-//        $result = Test::find()->where(['like','title','title'])->asArray()->all();
-        
-        //批量查询
-//        foreach(Test::find()->batch(1) as $test){
-//            print_r($test);
-//        }
-        
-//        print_r($result);
+//       Test::deleteAll('id>:id',array(':id'=>0));
     }
 
 }
